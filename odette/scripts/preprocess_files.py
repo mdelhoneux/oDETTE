c20036d813f9cf9bc5cb7eeb53b3c2d5cbd53730
mdelhoneux <mdelhoneux@gmail.com>
2016-02-22 09:46:03 +0100

    I was not using the conllx file in the ambig exp but the original file

--- odette/scripts/preprocess_files.py
+++ odette/scripts/preprocess_files.py
@@ -4,35 +4,37 @@ from src.treebank_transformer import TreebankTransformer
 from src.UD_treebank import UDtreebank
 
 def prepare_files(treebank_name, outdir=None, trainfile=None, testfile=None,
-                  ambig_type=None, dep_style='ud', pos_style='ud'):
-    if not outdir: outdir= config.exp + treebank_name
-    if not os.path.exists(outdir): os.mkdir(outdir)
-    if not trainfile and not testfile :
-        tb = UDtreebank(treebank_name)
-        trainfile = tb.trainfile
-        testfile = tb.devfile
-    TM = TreebankTransformer(treebank_name=treebank_name, dep_style=dep_style,
-                             pos_style=pos_style)
-    TM.transform(trainfile, TM.trainfile, 'to_conllx')
-    TM.transform(testfile, TM.testfile, 'to_conllx')
+				  ambig_type=None, dep_style='ud', pos_style='ud'):
+	if not outdir: outdir= config.exp + treebank_name
+	if not os.path.exists(outdir): os.mkdir(outdir)
+	if not trainfile and not testfile :
+		tb = UDtreebank(treebank_name)
+		trainfile = tb.trainfile
+		testfile = tb.devfile
+	TM = TreebankTransformer(treebank_name=treebank_name, dep_style=dep_style,
+							 pos_style=pos_style)
 
-    #experiments about ambiguity
-    if ambig_type:
-        TM.transform(trainfile, TM.trainfile, ambig_type)
-        TM.transform(testfile, TM.testfile, ambig_type)
+	TM.transform(trainfile, TM.trainfile, 'to_conllx')
+	TM.transform(testfile, TM.testfile, 'to_conllx')
+
+	#experiments about ambiguity
+	if ambig_type:
+		TM.transform(TM.trainfile, TM.trainfile, ambig_type)
+		TM.transform(TM.testfile, TM.testfile, ambig_type)
+
 
 if __name__=="__main__":
-    """usage: python preprocess_files.py treebank_name trainfile testfile (ambig)"""
-    import sys
-    treebank_name = sys.argv[1]
-    train = sys.argv[2]
-    test = sys.argv[3]
-    ambig = None
-    dep_style ="ud"
-    pos_style ="ud"
-    if len(sys.argv) > 4:
-        ambig = sys.argv[4]
-        dep_style = sys.argv[5]
-        pos_style = sys.argv[6]
-    prepare_files(treebank_name,trainfile=train,testfile=test, ambig_type=ambig,
-                  dep_style=dep_style, pos_style=pos_style)
+	"""usage: python preprocess_files.py treebank_name trainfile testfile (ambig)"""
+	import sys
+	treebank_name = sys.argv[1]
+	train = sys.argv[2]
+	test = sys.argv[3]
+	ambig = None
+	dep_style ="ud"
+	pos_style ="ud"
+	if len(sys.argv) > 4:
+		ambig = sys.argv[4]
+		dep_style = sys.argv[5]
+		pos_style = sys.argv[6]
+	prepare_files(treebank_name,trainfile=train,testfile=test, ambig_type=ambig,
+				  dep_style=dep_style, pos_style=pos_style)
