bc2249d7e201a4c75372fc524ba10d72a15613ca
mdelhoneux <mdelhoneux@gmail.com>
2016-04-07 16:03:11 +0200

    updated default

--- odette/config.py
+++ odette/config.py
@@ -5,5 +5,5 @@ maltparser = "./maltparser-1.8.1"
 data = "./Data/ud-treebanks-v1.2/"
 
 #options
-USE_CPOSTAG=False
+USE_CPOSTAG=True
 KEEP_COP=False #set to false to remove sentences with cop deprels
