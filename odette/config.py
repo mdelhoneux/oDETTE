927c33f86a6836403d0667cef34ced43c1ce12e3
mdelhoneux <mdelhoneux@gmail.com>
2016-04-07 14:51:39 +0200

    really updated config

--- odette/config.py
+++ odette/config.py
@@ -1,4 +1,9 @@
+#Directories
 exp="./EXP/"
 malteval = "./malteval"
 maltparser = "./maltparser-1.8.1"
 data = "./Data/ud-treebanks-v1.2/"
+
+#options
+USE_CPOSTAG=False
+KEEP_COP=False #set to false to remove sentences with cop deprels
