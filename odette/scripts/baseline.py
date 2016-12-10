2c07c001f391b1f0b97936e9338c75a4e393b840
mdelhoneux <mdelhoneux@gmail.com>
2016-12-10 14:24:40 +0100

    rmved error anal - not proper - should call all plot stuff but that's a TODO

--- odette/scripts/baseline.py
+++ odette/scripts/baseline.py
@@ -25,41 +25,16 @@ malteval = Malteval()
 def run_baseline_with_tagger(treebank_name,outdir=None,metric='LAS', parser="udpipe"):
     if not outdir: outdir= config.exp + treebank_name + "/"
     TM = TreebankManager(treebank_name,outdir=outdir, parser=parser)
-    #TODO: I might want to retrain it even if it's trained - then I should
-    #name it differently like with params or something
-    #TODO: make using a devfile an option
     if not TM._tagger.is_trained():
-        #TM.train_tagger(devfile=TM.treebank.devfile)
-        TM.train_tagger()
+        TM.train_tagger(devfile=TM.treebank.devfile)
     if not TM._parser.is_trained():
-        #TM.train_parser(devfile=TM.devfile)
-        TM.train_parser()
+        TM.train_parser(devfile=TM.devfile)
     TM.tag_test_file()
     TM.test_parser()
     uas, las= malteval.accuracy(TM.test_gold,TM.test_parsed)
     upos, xpos = pos_tagging_accuracy(TM.test_gold,TM.test_parsed)
     output = "%s;%s;%s;%s;%s\n"%(treebank_name,las,uas, upos,xpos)
     return output
-
-def error_analysis(treebank_name, outdir=None,parser="udpipe"):
-    #TODO: rename
-    TM = TreebankManager(treebank_name, outdir=outdir,parser=parser)
-    #do it only if file empty?
-    TM.tag_test_file()
-    TM.test_parser()
-    import numpy as np
-    lengths = [str(i) for i in np.arange(5,50,5)]
-    lass = []
-    for length in lengths:
-        uas, las= malteval.accuracy(TM.test_gold,TM.test_parsed,maxSenLen=length)
-        if las:
-            lass.append(las)
-    #get no lim
-    uas, las= malteval.accuracy(TM.test_gold,TM.test_parsed)
-    lass.append(las)
-    output = treebank_name +";" + ";".join(lass) + "\n"
-    return output
-
 
 def learning_curve(treebank_name,outdir=None,metric='LAS',parser='udpipe', split_sizes=None):
     #NOTE: not sure what happens if I send main in parallel and this again
@@ -88,7 +63,7 @@ def optimize_udpipe(treebank_name,outdir=None,metric='LAS',parser='udpipe', n_ru
     TM.tag_test_file()
     num_cores = min(multiprocessing.cpu_count(), n_runs)
     parser_name = deepcopy(TM._parser.name)
-    tms = Parallel(n_jobs=num_cores)(delayed(_one_run)(i, deepcopy(TM), parser_name) for i in range(n_runs))
+    tms = Parallel(n_jobs=num_cores)(delayed(_one_run)(i, deepcopy(TM), parser_name) for i in range(1,n_runs + 1))
     lass = []
     for tm in tms:
         uas, las= malteval.accuracy(tm.test_gold,tm.test_parsed)
@@ -101,8 +76,8 @@ def _one_run(i, TM, parser_name):
     TM._parser.name = TM.parser_name
     TM._parser.run = i
     if not TM._parser.is_trained():
-        TM._parser.train(TM.trainfile)
-        TM.test_parser()
+        TM._parser.train(TM.trainfile, TM.devfile)
+    TM.test_parser()
     return TM
 
 def _run_for_a_split(i, TM, parser_name):
@@ -116,6 +91,7 @@ def _run_for_a_split(i, TM, parser_name):
 
 def run_baseline(treebank_name, outdir=None, trainfile=None, testfile=None):
     if not outdir: outdir= config.exp + treebank_name
+    #TODO: this is kind of obsolete - rewrite
     TM = TreebankTransformer(treebank_name=treebank_name)
     """Train and parse"""
     train_gold = TM.trainfile
@@ -129,6 +105,7 @@ def run_baseline(treebank_name, outdir=None, trainfile=None, testfile=None):
     uas, las= malteval.accuracy(test_gold,parsed_baseline)
     output = "%s;%s;%s\n"%(treebank_name,las,uas)
     return output
+
 
 if __name__=="__main__":
     """usage: python baseline.py treebank_name (trainfile testfile)"""
